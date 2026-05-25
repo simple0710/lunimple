28e74e95efdc3e3cb14707f29786be311ecccd69
김찬민 <thesimpleman@naver.com>
2026-05-25 23:13:27 +0900

    init: backend init

---
+++ backend/src/test/java/com/lunimple/LunimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
